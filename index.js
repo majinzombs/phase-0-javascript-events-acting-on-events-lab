38a1ad1ab9c3ac4ef8062df8387eabc8444eec21
Jonathan Koban <jon.koban@outlook.com>
2022-04-14 16:13:22 -0400

    commit tree 988171581c3a1ac1547874f696e7a4c63d43165c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+// Style
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = '#FF69B4';
+dodger.style.bottom = '0px';
+
+// Move Functions Left
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace('px','');
+    const left = parseInt(leftNumbers, 10);
+
+    if(left > 0) {
+        dodger.style.left = `${left-1}px`;
+    }
+}
+
+// Move Function Right
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace('px','');
+    const left = parseInt(leftNumbers, 10);
+
+    if(left < 360) {
+        dodger.style.left = `${left+1}px`;
+    }
+}
+
+
+// Event Listener
+
+document.addEventListener('keydown', function(event){
+    if (event.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }else{
+        event.key === 'ArrowRight'
+        moveDodgerRight();
+    }
+});
